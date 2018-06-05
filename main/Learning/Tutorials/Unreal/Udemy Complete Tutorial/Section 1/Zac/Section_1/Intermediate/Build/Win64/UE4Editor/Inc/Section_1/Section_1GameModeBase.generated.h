33ad4a6a8f43e04ed36bdfed5c3d402977cd1e36
sewellza9@gmail.com <sewellza9@gmail.com>
2018-06-05 16:22:15 -0400

    Section 1 work of Udemy tutorial
    Contains the unreal project and scenes

---
+++ main/Learning/Tutorials/Unreal/Udemy Complete Tutorial/Section 1/Zac/Section_1/Intermediate/Build/Win64/UE4Editor/Inc/Section_1/Section_1GameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SECTION_1_Section_1GameModeBase_generated_h
+#error "Section_1GameModeBase.generated.h already included, missing '#pragma once' in Section_1GameModeBase.h"
+#endif
+#define SECTION_1_Section_1GameModeBase_generated_h
+
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_RPC_WRAPPERS
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASection_1GameModeBase(); \
+	friend SECTION_1_API class UClass* Z_Construct_UClass_ASection_1GameModeBase(); \
+public: \
+	DECLARE_CLASS(ASection_1GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Section_1"), NO_API) \
+	DECLARE_SERIALIZER(ASection_1GameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASection_1GameModeBase(); \
+	friend SECTION_1_API class UClass* Z_Construct_UClass_ASection_1GameModeBase(); \
+public: \
+	DECLARE_CLASS(ASection_1GameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Section_1"), NO_API) \
+	DECLARE_SERIALIZER(ASection_1GameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASection_1GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASection_1GameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASection_1GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASection_1GameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASection_1GameModeBase(ASection_1GameModeBase&&); \
+	NO_API ASection_1GameModeBase(const ASection_1GameModeBase&); \
+public:
+
+
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASection_1GameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASection_1GameModeBase(ASection_1GameModeBase&&); \
+	NO_API ASection_1GameModeBase(const ASection_1GameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASection_1GameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASection_1GameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASection_1GameModeBase)
+
+
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_12_PROLOG
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_RPC_WRAPPERS \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_INCLASS \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Section_1_Source_Section_1_Section_1GameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Section_1_Source_Section_1_Section_1GameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Section_1_Source_Section_1_Section_1GameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
