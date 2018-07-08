449294508a6f13f2ab1e9398c1a7fa65361fb925
sewellza9 <39471165+sewellza9@users.noreply.github.com>
2018-07-08 18:16:07 -0400

    Section 75 push

---
+++ main/Learning/Tutorials/Unreal/Udemy Complete Tutorial/Section 3/Zac/Section3/Source/Section3/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// NUNYA
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+AActor* owner;
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	owner = GetOwner();
+
+	FRotator newRotation = FRotator(0, -80, 0);
+
+	owner->SetActorRotation(newRotation);
+
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
